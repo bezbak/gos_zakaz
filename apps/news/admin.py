8f19b2bb18cb51275c25f01f5d07e5b44ab7f54b
itek <itybek@gmail.com>
2023-07-27 22:41:26 +0600

    update admin panel

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django import forms
 from apps.news.models import News, Media
 # Register your models here.
-admin.site.register(News)
+class MediaForm(forms.ModelForm):
+    class Meta:
+        model = Media
+        fields = (
+            'image',
+            'news'
+        )
+class MediaInline(admin.TabularInline):
+    form = MediaForm
+    model = Media
+    extra = 2
+    
+class NewsAdmin(admin.ModelAdmin):
+    inlines = [
+        MediaInline,
+    ]
+
+
+admin.site.register(News, NewsAdmin)
 admin.site.register(Media)
