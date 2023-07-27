da74b1e74a8991d9537e421a71a6b803283d540f
itek <itybek@gmail.com>
2023-07-27 22:36:16 +0600

    add media

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.news.models import News
+from apps.news.models import News, Media
 # Register your models here.
 admin.site.register(News)
+admin.site.register(Media)
