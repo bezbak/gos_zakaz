c14d3c46f5a5d215e6988ca13376fae9c779f52d
bezbak <Cemetey.2019@gmail.com>
2023-08-17 10:56:12 +0600

    pre it park version

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from apps.pages.models import Police, DevFund, StateSymbols
 # Register your models here.
--- static/scripts/slider.js
+++ static/scripts/slider.js
@@ -1,1 +1,47 @@
-$('.news_slider').slick();
+// $('.news_slider').slick();
+$(document).ready(function() {
+    $('.news_slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const modalImage = document.querySelector('.modal-image');
+    const closeBtn = document.querySelector('.close-btn');
+
+    $('.slider-item').on('click', function() {
+        const imgSrc = $(this).find('img').attr('src');
+        modalImage.setAttribute('src', imgSrc);
+        modalOverlay.style.display = 'flex';
+    });
+
+    $(closeBtn).on('click', function() {
+        modalOverlay.style.display = 'none';
+    });
+});
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        // Здесь выполняйте действия при нажатии клавиши "Esc"
+        // Например, закрытие модального окна
+        closeModal();
+    }
+});
+
+function closeModal() {
+    // Ваш код для закрытия модального окна
+    // Например:
+    const modalOverlay = document.querySelector('.modal-overlay');
+    modalOverlay.style.display = 'none';
+}
