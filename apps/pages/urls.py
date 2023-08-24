e73e3ad684d409d4905039787d80aa4a5e6262ab
bezbak <Cemetey.2019@gmail.com>
2023-08-24 17:33:43 +0600

    add rayons

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.pages.views import policy, devfund, state_symbols
+from apps.pages.views import policy, devfund, state_symbols,rayon_detail
 
 urlpatterns = [
     path('policy/', policy, name='policy'),
     path('devfund/', devfund, name='devfund'),
     path('state_symbols/', state_symbols, name='state_symbols'),
+    path('rayon_detail/<int:id>/', rayon_detail, name='rayon_detail'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pages.context_processors.rayons_context'
+                'apps.pages.context_processors.rayons_context'
             ],
         },
     },
